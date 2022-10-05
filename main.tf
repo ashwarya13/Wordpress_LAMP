9c252fcc41190c86ab2536089fe53a029d5daa5f
Ashwarya <ashwaryavikram13@gmail.com>
2022-10-05 13:46:13 +0000

    SSH key deployment changes

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ locals {
   ami_id = "ami-09e67e426f25ce0d7"
   vpc_id = aws_default_vpc.defaultvpc.id
   ssh_user = "ubuntu"
-  key_name = "demo"
   private_key_path = "${path.cwd}/demo.pem"
 }
 
@@ -15,6 +14,14 @@ resource "aws_default_vpc" "defaultvpc" {
   tags = {
     Name = "Default VPC"
   }
+}
+
+resource "aws_key_pair" "demo" {
+
+  key_name = "demo"
+
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDA1gPO1evCLPHn4hGTeyK5qrDJNRWsMOoGyfPO26x5Wwcr5ye1vcfWEFeNJAnYpoAK6lo2ygOEqX0AHlFho+XPjp1ZwCLemA+P+w4h5X3bmzmSZW8eyyT6jHy7SzXoANE3KUL4IWK9ojcyMVuuxW0c6o9mia9Y5TFYqxZ9ZWQuAzGa9R9A5NXNYZpiausJeODIOtBmY6o69DSovE3VbFznvS99QWoJuWyF2Lp7mq/nwFqwSroYxh/lmYhz/XQ5BXFu8wibTkvK0/eDUp2/vysUnzMmImJALbJoe1UcZd3SCCiJUyh3TUMmn7LaXj6qWAZqTXgUywBeZJqxGeKITnUzhSIkXaQvLsnnQTkiMiR8dcNR1Os0g2gW+bMxjE3094F1aVYGzNGQ8+9fkPWMrnpLmBFB0N5jrgiKL4lCjFrs7BwxlqScPM05U7rUL4iN+pluzhKcYu3x9UhaFLmyQCE1Oi2t/t/uMYFH1nGjecmynm2dh7SaZ499V5w57ruKEwU= labsuser@ip-172-31-56-96"
+
 }
 
 resource "aws_security_group" "demoaccess" {
@@ -46,7 +53,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   vpc_security_group_ids =[aws_security_group.demoaccess.id]
-  key_name = local.key_name
+  key_name = aws_key_pair.demo.key_name
 
   tags = {
     Name = "Wordpress ec2"
